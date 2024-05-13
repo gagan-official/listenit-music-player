6cc4ef5c0817a298a1d8ffa117b0b77ba599c5da
Gagandeep Singh <gagan.official2k1@gmail.com>
2024-05-13 21:36:27 +0530

    Modified backend file structure

---
+++ backend/api/index.js
@@ -0,0 +1,24 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("data.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+
+server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/product/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
+
+server.listen(port, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
+
+// Used reference from this repo: https://github.com/SofiDevO/alurageek-API/blob/sofidev/README.md
